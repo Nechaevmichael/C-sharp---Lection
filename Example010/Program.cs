595338968c8e3a64156cb8f9a5c69a6830f5a226
NechaevMichael <nechaevmichael@mail.ru>
2022-09-04 14:15:11 +0500

    add Example010

---
+++ Example010/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 2, 3, 45, 4, 12, 15, 42, 34, 45 };
+
+int n = array.Length;
+
+int find = 45;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index += 1; // или index++;
+}
